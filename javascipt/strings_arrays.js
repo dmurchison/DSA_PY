35644e6f4fafbecc4a25a6d5d23cee3f409ccc5f
dmurchison <duncancmurchison@gmail.com>
2022-11-14 18:47:40 -0500

    added_comments

--- javascipt/strings_arrays.js
+++ javascipt/strings_arrays.js
@@ -33,7 +33,11 @@ console.log(uncompress("127y")); // ->'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
 console.log('-------');
 
 
-
+// COMPRESS
+// Write a function, compress, that takes in a string as an argument. 
+// The function should return a compressed version of the string where consecutive occurrences 
+// of the same characters are compressed into the number of occurrences followed by the character. 
+// Single character occurrences should not be changed.
 
 const compress = (s) => {
   // todo
