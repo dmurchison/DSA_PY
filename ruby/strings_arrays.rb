2ae756ea88818acd794b70bb65b6205913039622
dmurchison <duncancmurchison@gmail.com>
2022-11-14 13:07:12 -0500

    commited comments

--- ruby/strings_arrays.rb
+++ ruby/strings_arrays.rb
@@ -5,23 +5,24 @@
 # for example, '2c' or '3a'.
 # The function should return an uncompressed version of the string where each 'char' of a group is repeated 'number' times consecutively. 
 # You may assume that the input string is well-formed according to the previously mentioned pattern.
-
+# 2 POINTER METHOD
 def uncompress(s)
-  result = []
-  numbers = '0123456789'
-  i = 0
-  j = 0
-  while j < s.length
+  result = [] # setup array for the final result to be joined at the end of the end of the function
+  numbers = '0123456789' # numbers string to test with the .include? method
+  i = 0 # i will be the first pointer representing the first part of the number
+  j = 0 # j will be the second pointer representing the second digit and the letter.
+  while j < s.length # since j is the second pointer we will use this as the while loop
     if numbers.include?(s[j])
-      j += 1
+      j += 1 # if s[j] is a number than just concatonate
     else
-      res = s.slice(i, j).to_i
-      res.times { result.push(s[j]) }
+      res = s.slice(i, j).to_i # else (once the j is on a letter) slice from i to j non inclusive and use the to_i method to change it to an integer
+      res.times { result.push(s[j]) } # That number amount of times, push the letter (currently s[j]) into the result array created at the beginning of the function
       j += 1
       i = j
+      # concat j again and bring i up to speed
     end
   end
-  result.join('')
+  return result.join('') # join the result together and return it!
 end
 
 p uncompress("2c3a1t") # -> 'ccaaat'
