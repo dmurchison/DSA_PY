f513bd496520deede415d8f79fb48687d77d14c5
dmurchison <duncancmurchison@gmail.com>
2022-11-14 19:25:12 -0500

    js_anagrams

--- javascipt/strings_arrays.js
+++ javascipt/strings_arrays.js
@@ -70,3 +70,35 @@ console.log('-------');
 
 
 
+const anagrams = (s1, s2) => {
+  const count = {};
+  for (let char of s1) {
+    if (!(char in count)) {
+      count[char] = 0;
+    }
+    count[char] += 1;
+  }
+  
+  for (let char of s2) {
+    if (count[char] === undefined) {
+      return false;
+    } else {
+      count[char] -= 1;
+    }
+  }
+  
+  for (let char in count) {
+    if (count[char] !== 0) {
+      return false;
+    }
+  }
+  
+  return true;
+};
+
+
+
+
+
+
+
