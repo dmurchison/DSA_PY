8680b45c148bf250f1411b4780f98d1f821e3e2b
dmurchison <duncancmurchison@gmail.com>
2022-11-14 19:35:25 -0500

    most_frequent_char

--- ruby/strings_arrays.rb
+++ ruby/strings_arrays.rb
@@ -94,3 +94,10 @@ p anagrams('pp', 'oo') # -> False
 
 
 
+
+# MOST FREQUENT CHAR
+# Write a function, most_frequent_char, that takes in a string as an argument. 
+# The function should return the most frequent character of the string. 
+# If there are ties, return the character that appears earlier in the string.
+
+
