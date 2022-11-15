b5127005d5bc77fac31256c204bfe376f420c530
Duncan Murchison <duncancmurchison@gmail.com>
2022-11-15 18:45:21 -0500

    pair_sum

--- ruby/strings_arrays.rb
+++ ruby/strings_arrays.rb
@@ -156,10 +156,8 @@ def most_frequent_char(s)
   s.each_char do |el|
     hash[el] += 1
   end
-  hash.max_by{ |k,v| v }
+  hash.max_by{ |k,v| v }[0]
 end
-
-
 
 p most_frequent_char('bookeeper') # -> 'e'
 p most_frequent_char('david') # -> 'd'
@@ -172,4 +170,27 @@ p most_frequent_char('riverbed') # -> 'r'
 
 
 
+# PAIR SUM
+# Write a function, pair_sum, that takes in a list and a target sum as arguments. 
+# The function should return a tuple containing a pair of indices whose elements sum to the given target. 
+# The indices returned must be unique.
 
+# Be sure to return the indices, not the elements themselves.
+# There is guaranteed to be one such pair that sums to the target.
+
+def pair_sum(numbers, target_sum)
+  tuple = []
+  numbers.each_with_index do |el1, idx1|
+    numbers.each_with_index do |el2, idx2|
+      return [idx1, idx2] if idx1 < idx2 && el1 + el2 == target_sum
+    end
+  end
+  return nil
+end
+
+p pair_sum([3, 2, 5, 4, 1], 8) # -> (0, 2)
+p pair_sum([4, 7, 9, 2, 5, 1], 5) # -> (0, 5)
+p pair_sum([4, 7, 9, 2, 5, 1], 3) # -> (3, 5)
+p pair_sum([1, 6, 7, 2], 13) # -> (1, 2)
+p pair_sum([9, 9], 18) # -> (0, 1)
+p pair_sum([6, 4, 2, 8 ], 12) # -> (1, 3)
