168056a392bdd87c6df0cc4b819d704152a09c5b
Duncan Murchison <duncancmurchison@gmail.com>
2022-11-15 21:34:57 -0500

    intersection2

--- ruby/strings_arrays.rb
+++ ruby/strings_arrays.rb
@@ -256,11 +256,15 @@ def intersection(list1, list2)
   return result.sort
 end
 
+def intersection2(list1, list2)
+  list1 & list2
+end
+
 p 'INTERSECTION'
-p intersection([4,2,1,6], [3,6,9,2,10]) # -> [2,6]
-p intersection([2,4,6], [4,2]) # -> [2,4]
-p intersection([4,2,1], [1,2,4,6]) # -> [1,2,4]
-p intersection([0,1,2], [10,11]) # -> []
+p intersection2([4,2,1,6], [3,6,9,2,10]) # -> [2,6]
+p intersection2([2,4,6], [4,2]) # -> [2,4]
+p intersection2([4,2,1], [1,2,4,6]) # -> [1,2,4]
+p intersection2([0,1,2], [10,11]) # -> []
 a = Array (0..5000)
 b = Array (0..5000)
 # p intersection(a, b) # -> [0,1,2,3,..., 4999]
