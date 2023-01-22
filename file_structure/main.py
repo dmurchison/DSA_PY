9d6a7996fd00058be4857930720766abf3789a8d
Duncan Murchison <duncancmurchison@gmail.com>
2023-01-22 17:41:13 -0500

    quick fix

--- file_structure/main.py
+++ file_structure/main.py
@@ -10,11 +10,16 @@ f.write('This is going to be the third line, appended.\n')
 f.write('This is the final line, appended.\n')
 f.close()
 
+
+# Possible to open a file in read mode
 f = open('file_structure/test_file.txt', 'r')
-file_line = f.readline()
-print(file_line)
-file_line = f.readline()
-print(file_line)
+for line in f.readlines():
+    print(line)
 f.close()
+
+# Preferred way to open a file
+with open('file_structure/test_file.txt', 'r') as f:  # f = open(..)
+    for line in f.readlines():
+        print(line)
 
 
