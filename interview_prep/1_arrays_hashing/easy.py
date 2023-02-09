e2f1ec895cc3ad7af55e5129e7d021bd62ce25b7
dmurchison <duncancmurchison@gmail.com>
2023-02-09 03:23:50 -0500

    commiting for the day

--- interview_prep/1_arrays_hashing/easy.py
+++ interview_prep/1_arrays_hashing/easy.py
@@ -10,21 +10,10 @@ from typing import List
 
 class Solution:
     def contains_duplicate(self, nums: List[int]) -> bool:
-        hash_set = set()
-        for n in nums:
-            if n in hash_set:
-                return True
-            hash_set.add(n)
-        return False
-
-    def contains_duplicate_2(self, nums: List[int]) -> bool:
-        print(len(set(nums)))
-        print(len(nums))
+        
 
 
-print(Solution().contains_duplicate_2([1,2,3,1])) # True
-print(Solution().contains_duplicate_2([1,2,3,4])) # False
-print()
+print(my_func())
 print(Solution().contains_duplicate([1,2,3,1])) # True
 print(Solution().contains_duplicate([1,2,3,4])) # False
 print(Solution().contains_duplicate([1,1,1,3,3,4,3,2,4,2])) # True
@@ -39,33 +28,55 @@ print()
 # Given two strings s and t , write a function to determine if t is an anagram
 # of s.
 
-class Solution:
-    def is_anagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        hash_map = {}
-        for c in s:
-            if c in hash_map:
-                hash_map[c] += 1
-            else:
-                hash_map[c] = 1
-        for c in t:
-            if c not in hash_map:
-                return False
-            hash_map[c] -= 1
-            if hash_map[c] < 0:
-                return False
-        return True
+# class Solution:
+#     def is_anagram(self, s: str, t: str) -> bool:
 
-print(Solution().is_anagram("anagram", "nagaram")) # True
-print(Solution().is_anagram("rat", "car")) # False
-print(Solution().is_anagram("a", "ab")) # False
-print(Solution().is_anagram("ab", "a")) # False
-print(Solution().is_anagram("aacc", "ccac")) # False
-print(Solution().is_anagram("racecar", "racecar")) # True
-print()
+
+# print(Solution().is_anagram("anagram", "nagaram")) # True
+# print(Solution().is_anagram("rat", "car")) # False
+# print(Solution().is_anagram("a", "ab")) # False
+# print(Solution().is_anagram("ab", "a")) # False
+# print(Solution().is_anagram("aacc", "ccac")) # False
+# print(Solution().is_anagram("racecar", "racecar")) # True
+# print()
 
 
 
+
+# Replace Elements with Greatest Element on Right Side
+# Given an array arr, replace every element in that array with the greatest
+# element among the elements to its right, and replace the last element with
+# -1.
+
+# class Solution:
+#     def replace_elements(self, arr: List[int]) -> List[int]:
+
+
+# print(Solution().replace_elements([17,18,5,4,6,1])) # [18,6,6,6,1,-1]
+# print(Solution().replace_elements([400])) # [-1]
+# print(Solution().replace_elements([400, 400])) # [400, -1]
+# print(Solution().replace_elements([400, 400, 400])) # [400, 400, -1]
+# print(Solution().replace_elements([400, 400, 400, 400])) # [400, 400, 400, -1]
+# print(Solution().replace_elements([400, 400, 400, 400, 400])) # [400, 400, 400, 400, -1]
+# print(Solution().replace_elements([400, 400, 400, 400, 400, 400])) # [400, 400, 400, 400, 400, -1]
+# print()
+
+
+
+# Is Subsequence
+# Given two strings s and t, check if s is a subsequence of t.
+
+# class Solution:
+#     def is_subsequence(self, s: str, t: str) -> bool:
+
+
+# print(Solution().is_subsequence("abc", "ahbgdc")) # True
+# print(Solution().is_subsequence("axc", "ahbgdc")) # False
+# print(Solution().is_subsequence("acb", "ahbgdc")) # False
+# print(Solution().is_subsequence("abc", "abc")) # True
+# print(Solution().is_subsequence("abc", "ab")) # False
+# print(Solution().is_subsequence("abc", "a")) # False
+# print(Solution().is_subsequence("abc", "")) # False
+# print()
 
 
