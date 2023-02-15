54bc578589f998501db1ce94a82eb4fa233e25b6
Duncan Murchison <duncancmurchison@gmail.com>
2023-02-15 16:19:35 -0500

    added some notes

--- interview_prep/1_arrays_hashing/easy.py
+++ interview_prep/1_arrays_hashing/easy.py
@@ -10,11 +10,18 @@ from typing import List
 
 class Solution:
     def contains_duplicate(self, nums: List[int]) -> bool:
-        hash_set = set()
-        for n in nums:
-            if n in hash_set:
+        # Create an empty set, a set is basically a dictionary without values.
+        # The rule of not having any duplicates still applies.
+        mySet = set()
+        # Iterate through the nums array.
+        for i in nums:
+            # Check if the set contains the number already.
+            if i in mySet:
+                # Once the set has a duplicate we exit the function by returning True.
                 return True
-            hash_set.add(n)
+            # Still iterating through nums; add each iterable to the set.
+            mySet.add(i)
+        # If the loop finishes without returning true, we know there were no duplicates.
         return False
 
 
@@ -34,10 +41,20 @@ print()
 
 class Solution:
     def is_anagram(self, s: str, t: str) -> bool:
-        my_hash = {}
-        for i in s:
-            my_hash[i] = 1 + my_hash.get(i, 0)
-        return my_hash
+        # First check if s and t strings are the same length.
+        if len(s) != len(t):
+            return False
+        # Create two hashes to represent each string.
+        hashS, hashT = {}, {}
+
+        # Add values to the hashes by iterating through the length of the strings
+        # and incrementing the values by 1.
+        for i in range(len(s)):
+            # Set the value of the current key to 1 + the original value of that key
+            hashS[s[i]] = 1 + hashS.get(s[i], 0)
+            hashT[t[i]] = 1 + hashT.get(t[i], 0)
+        # Now that we have two hash_counters representing each string, return this boolean.
+        return hashS == hashT
 
 
 print(Solution().is_anagram("anagram", "nagaram")) # True
@@ -50,6 +67,26 @@ print()
 
 
 
+# Replace Elements with Greatest Element on Right Side
+# Given an array arr, replace every element in that array with the greatest 
+# element among the elements to its right, and replace the last element with -1.
+# After doing so, return the array.
+
+class Solution:
+    def right_side_list(self, nums: List[int]) -> List[int]:
+        # Set -1 to a variable to be used during the iteration.
+        rightMax = -1
+        # Iterate backwards through the nums array.
+        for i in range(len(nums) - 1, -1, -1):
+            # Max of nums[i] and the current rightMax gets saved to newMax var.
+            newMax = max(rightMax, nums[i])
+            # Replace nums[i] with the highest value to the right.
+            nums[i] = rightMax
+            # Set the new rightMax to be the result of the last number and start the loop over.
+            rightMax = newMax
+        return nums
 
 
+print(Solution().right_side_list([1,12,3,5,-5,2])) # [12,5,5,2,2,-1]
+print(Solution().right_side_list([8,5,10,22,-14,12,5,8])) # [22,22,22,12,12,8,8,-1]
 
