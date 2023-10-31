044ff283812453487e5b17eb014eb8fa04d85f7c
Duncan Murchison <duncan@computeranything.dev>
2023-10-30 20:38:42 -0400

    🚀 fix upgrades to pyenv

--- python/testing/exceptions.py
+++ python/testing/exceptions.py
@@ -4,22 +4,25 @@ print(sample_dict)
 
 try:
     print("This will work", sample_dict["a"])
-    print("This will not work", sample_dict["d"])
+    # print("This will not work", sample_dict["d"])
 except KeyError as e:
     print("Oh no, you tried to access a key that doesn't exist!", str(e))
-except Exception as e:
-    print("Oh no, don't be an idiot!", str(e))
+finally:
+    print("This will always run")
 
+print("We made it to the end!")
 
 a = 1
 b = 0
+c = None
 try:
     if b == 0:
         raise ZeroDivisionError("You can't divide by zero!")
-    c = a / b
-except KeyError as e:
-    print("Oh no, you tried to access a key that doesn't exist!", str(e))
+    c = a / 1
+except ZeroDivisionError as e:
+    print("Oh no, you tried to divide by zero!", str(e))
 
-
+# print(a / b)
 print("We made it to the end!")
+
 
