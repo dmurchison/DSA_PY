327371b78f91dea0e1929ad17d94e35de09b40be
Duncan Murchison <duncan@computeranything.dev>
2024-03-18 19:26:48 -0400

    🚀 New

---
+++ PyPrep/data_structures/classes/lesson_1.py
@@ -0,0 +1,19 @@
+
+def cube_list(numbers) -> list[int]:
+    """
+    Returns a list of the cubes of the input numbers.
+
+    Args:
+        numbers (List[int]): The input list of numbers.
+
+    Returns:
+        List[int]: The list of the cubes of the input numbers.
+    """
+    return [num ** 3 for num in numbers]
+
+print(cube_list([1,2,3,4,5,6,7,8,9,10]))
+
+characters = []
+# Use an empty list characters and a += augmented assignment statement to convert the string 'Birthday' into a list of characters.
+characters += 'Birthday'
+print(characters)
