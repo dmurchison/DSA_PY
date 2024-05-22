f3d1e6d196468e11cf51ef352cf2f8ed6f7e28cc
Duncan Murchison <duncan@computeranything.dev>
2024-05-22 16:41:49 -0400

    😍 im loving python

--- PyPrep/data_structures/lists.py
+++ PyPrep/data_structures/lists.py
@@ -3,6 +3,10 @@
 # Allows duplicate members.
 
 example_list = [1,2,3]
-print(example_list, type(example_list))
-example_list.append("Hello World")
+
 print(example_list)
+
+example_list.append("Hello World")
+
+print(example_list, type(example_list), len(example_list))
+
