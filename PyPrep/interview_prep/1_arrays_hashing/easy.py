ee1cae96a64379141718628d762509563f44ff67
Duncan Murchison <duncan@computeranything.dev>
2024-05-25 14:24:46 -0400

    anagramz

--- PyPrep/interview_prep/1_arrays_hashing/easy.py
+++ PyPrep/interview_prep/1_arrays_hashing/easy.py
@@ -14,3 +14,15 @@ class DuplicateSolution:
 print(DuplicateSolution().contains_dup([1,2,3,3,4,5]))
 print(DuplicateSolution().contains_dup([1,2,3,4,5]))
 
+class Anagram:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        hash_s, hash_t = {}, {}
+        for i in range(len(s)):
+            hash_s[s[i]] = 1 + hash_s.get(s[i], 0)
+            hash_t[t[i]] = 1 + hash_t.get(t[i], 0)
+        return hash_s == hash_t
+
+print(Anagram().isAnagram("anagram", "nagaram"))
+
