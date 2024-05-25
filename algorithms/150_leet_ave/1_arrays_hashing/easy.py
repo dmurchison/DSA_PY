9a2aee629e1df9a2bd37ccc5c1a9d9eb406c20ce
Duncan Murchison <duncan@computeranything.dev>
2024-05-25 18:05:27 -0400

    ❤️ b_search fails

--- algorithms/150_leet_ave/1_arrays_hashing/easy.py
+++ algorithms/150_leet_ave/1_arrays_hashing/easy.py
@@ -2,17 +2,28 @@ from typing import List
 
 # Easy Array Hashing Problems
 
-class DuplicateSolution:
+class ContainsDuplicateSolution:
     def contains_dup(self, nums: List[int]) -> bool:
-        set_checker = set()
+        """ Time: O(n)
+            Space: O(n)
+
+        Args:
+            nums (List[int]): List to check
+
+        Returns:
+            bool: True if nums contains duplicate
+        """
+        hash_set = set()
         for n in nums:
-            if n in set_checker:
+            if n in hash_set:
                 return True
-            set_checker.add(n)
+            hash_set.add(n)
         return False
 
-print(DuplicateSolution().contains_dup([1,2,3,3,4,5]))
-print(DuplicateSolution().contains_dup([1,2,3,4,5]))
+print(ContainsDuplicateSolution().contains_dup([1,2,3,3,4,5])) # True
+print(ContainsDuplicateSolution().contains_dup([1,2,3,4,5])) # False
+print()
+
 
 class AnagramSolution:
     def isAnagram(self, s: str, t: str) -> bool:
@@ -24,7 +35,10 @@ class AnagramSolution:
             hash_t[t[i]] = 1 + hash_t.get(t[i], 0)
         return hash_s == hash_t
 
-print(AnagramSolution().isAnagram("anagram", "nagaram"))
+print(AnagramSolution().isAnagram("anagram", "nagaram")) # True
+print(AnagramSolution().isAnagram("anagram", "Hello")) # False
+print()
+
 
 class TwoSumSolution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
@@ -37,4 +51,4 @@ class TwoSumSolution:
         Returns:
             List[int]: _description_
         """
-        # make an hashmap 
+        # make a hashmap
