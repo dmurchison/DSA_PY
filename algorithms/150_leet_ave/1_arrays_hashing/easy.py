538fe016ca5aec1c52af35de1ce35ba61157ad12
Duncan Murchison <duncan@computeranything.dev>
2024-06-14 18:28:48 -0400

    🐼 more pandas code

--- algorithms/150_leet_ave/1_arrays_hashing/easy.py
+++ algorithms/150_leet_ave/1_arrays_hashing/easy.py
@@ -52,3 +52,4 @@ class TwoSumSolution:
             List[int]: _description_
         """
         # make a hashmap
+
--- panda.py
+++ panda.py
@@ -1,15 +1,21 @@
 import pandas as pd
 
-df = pd.DataFrame({'A':[10,20,30,40], 'B':[15,25,35,45]})
+
+df = pd.DataFrame({'A': [10, 20, 30, 40], 'B': [15, 25, 35, 45]})
+
 df['C'] = df['A'] + df['B']
 df['D'] = df['C'].apply(lambda x: x/2 if x>50 else x)
-
 max_value = df['D'].max()
 
-grouped_df = df.groupby('A')
-
-mean_df = grouped_df.mean()
-mean_df1 = grouped_df.apply(lambda x: x.mean())
-
 print(max_value)
+print()
 
+
+df2 = pd.DataFrame({'A': ['foo', 'bar', 'foo', 'bar'],
+     'B': [1, 2, 3, 4],
+     'C': [5, 6, 7, 8]})
+
+df22 = df2.groupby('A')
+mean_df = df22.mean()
+
+print(mean_df)
